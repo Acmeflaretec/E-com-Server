4111bd4a235085fb0432db0385e52192679caf97
ANJITH B <anjithb200@gmail.com>
2024-05-29 11:22:24 +0530

    auth routes & controllers defined, user model added

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,10 @@
 const express = require('express');
+const authRoutes = require('./authRoutes');
 const productRoutes = require('./productRoutes');
 const categoryRoutes = require('./categoryRoutes');
 const router = express.Router();
 
+router.use('/v1/auth', authRoutes);
 router.use('/v1/category', categoryRoutes);
 router.use('/v1/products', productRoutes);
 
